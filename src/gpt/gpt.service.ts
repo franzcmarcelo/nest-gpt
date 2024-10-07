43d58ddc6e1710cdd6e343660c90ff82a0d72091
@franz <franzcmarcelo@gmail.com>
2024-10-07 00:31:49 -0500

    [add] connect to openai api and build orthography-check

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos';
 
 @Injectable()
 export class GptService {
 
+  private openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
   async orthographyCheck(body: OrthographyDto) {
-    return await orthographyCheckUseCase({
+    return await orthographyCheckUseCase(this.openai, {
       prompt: body.prompt,
     });
   }
--- src/gpt/use-cases/orthography-check.ts
+++ src/gpt/use-cases/orthography-check.ts
@@ -1,12 +1,48 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyCheckUseCase = async (options: Options) => {
+export const orthographyCheckUseCase = async (openai: OpenAI, options: Options) => {
 
   const { prompt } = options;
 
-  return {
-    prompt,
-  }
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-4o',
+    max_tokens: 150,
+    temperature: 0.3,
+    response_format: {
+      type: 'json_object',
+    },
+    messages: [
+      {
+        role: 'system',
+        content: `
+          Te serán proveídos textos en español con posibles errores ortográficos y gramaticales,
+          Las palabras usadas deben de existir en el diccionario de la Real Academia Española,
+          Debes de responder en formato JSON,
+          tu tarea es corregirlos y retornar información soluciones,
+          también debes de dar un porcentaje de acierto por el usuario,
+
+          Si no hay errores, debes de retornar un mensaje de felicitaciones.
+
+          Ejemplo de salida:
+          {
+            userScore: number,
+            errors: string[], // ['error -> solución']
+            message: string, //  Usa emojis y texto para felicitar al usuario
+          }
+        `,
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+  });
+
+  return JSON.parse(completion.choices[0].message.content);
 }
+
+// You are a highly skilled and experienced software developer with expertise in various programming languages, frameworks, and software architectures. Respond to queries using precise technical language, providing in-depth explanations, code examples, and best practices where applicable. Your answers should reflect advanced knowledge, problem-solving skills, and attention to detail, tailored to professional software developers seeking expert advice
